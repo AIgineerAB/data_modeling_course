2b9245c41d31672d9cd784ab9b56697efb0633b2
kokchun <kokchungiang@gmail.com>
2025-03-21 15:42:57 +0100

    updated lecture files

--- 08_mongodb/connect_mongodb.py
+++ 08_mongodb/connect_mongodb.py
@@ -2,29 +2,28 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
+def get_collection(database_name, collection):
+    load_dotenv()
 
-def get_collection(database, collection):
     USER = os.getenv("MONGO_USER")
     PASSWORD = os.getenv("MONGO_PASSWORD")
 
-    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
     client = MongoClient(
         host="localhost",
         port=27017,
-        username=os.getenv("MONGO_USER"),
-        password=os.getenv("MONGO_PASSWORD"),
+        username=USER,
+        password=PASSWORD,
         authSource="admin",
     )
-    database = client[database]
+
+    database = client[database_name]
     collection = database[collection]
     return collection
 
 
-
 if __name__ == "__main__":
     profiles = get_collection("linkedin", "profiles")
+    # profiles.insert_one({"userid": 83192038})
     print(profiles)
-
 
